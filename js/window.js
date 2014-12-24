492fe1f2a2ab364b89fd8e028e817ee70a7f1841
Cungen <1360571582@qq.com>
2014-12-24 18:01:25 +0800

    Init commit.

---
+++ js/window.js
@@ -0,0 +1,25 @@
+/**
+ * window event
+ */
+(function() {
+    var startX = 0,
+        startY = 0;
+    $('.window .title-bar').on('mousedown', function(event) {
+        event.preventDefault();
+        var win = $(this).parents('.window');
+        startX = event.pageX - win.position().left;
+        startY = event.pageY - win.position().top;
+        $('body').bind('mousemove', function(e) {
+            e.preventDefault();
+            win.css({
+                left: e.pageX - startX + 'px',
+                top: e.pageY - startY + 'px'
+            });
+        });
+    });
+
+    $('body').on('mouseup', function() {
+        $('body').unbind('mousemove');
+    });
+
+})();
